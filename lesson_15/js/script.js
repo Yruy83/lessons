fd2863f04678d86b7f69123be1f7656170c171bb
Yruy <119973906+Yruy83@users.noreply.github.com>
2024-08-20 00:57:32 +0300

    Delete lesson_15 directory

--- lesson_15/js/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('click', function (e) {
-	const targetElement = e.target;
-	if (targetElement.closest('.burger-button')) {
-		document.documentElement.classList.toggle('menu-open');
-	}
-})
